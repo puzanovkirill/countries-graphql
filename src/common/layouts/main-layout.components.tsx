a8eee175fae63ebb2001df5ad9d2751d4438d6bb
kirillpuzanov <k.puzanov@3divi.com>
2022-07-01 13:22:40 +0500

    feat: added basic page positioning

--- src/common/layouts/main-layout.components.tsx
+++ src/common/layouts/main-layout.components.tsx
@@ -6,12 +6,21 @@ import {
   GridItem,
   VStack,
   useColorModeValue,
+  Flex,
+  Avatar,
+  Text,
+  Icon,
+  useColorMode,
 } from '@chakra-ui/react';
-import { House } from 'phosphor-react';
+import { House, Moon, Sun } from 'phosphor-react';
 import { Outlet } from 'react-router-dom';
 
 function MainLayout() {
-  const navBg = useColorModeValue('gray.300', 'gray.600');
+  const { toggleColorMode } = useColorMode();
+  const text = useColorModeValue('dark', 'light');
+  const SwitchIcon = useColorModeValue(Moon, Sun);
+  const primaryBg = useColorModeValue('gray.300', 'gray.600');
+
   return (
     <Grid
       templateAreas='"header header" "nav main"'
@@ -20,8 +29,26 @@ function MainLayout() {
       gap={0}
       h="100vh"
     >
-      <GridItem pl="2" bg="orange.300" area="header" h={14}></GridItem>
-      <GridItem pl="2" bg={navBg} area="nav" p={0} w={20}>
+      <GridItem pl="2" bg={primaryBg} area="header" h={14} p={0} px={4}>
+        <Flex justifyContent="space-between" alignItems="center" h="full">
+          <HStack alignItems="center">
+            <Avatar />
+            <Text>Powered by Kirill Puzanov</Text>
+          </HStack>
+          <Tooltip label={`Switch to ${text} mode`}>
+            <IconButton
+              size="md"
+              fontSize="lg"
+              variant="ghost"
+              color="current"
+              onClick={toggleColorMode}
+              icon={<Icon as={SwitchIcon} w="6" h="6" />}
+              aria-label={`Switch to ${text} mode`}
+            />
+          </Tooltip>
+        </Flex>
+      </GridItem>
+      <GridItem pl="2" bg={primaryBg} area="nav" p={0} w={20}>
         <VStack p={4}>
           <Tooltip label="Home">
             <IconButton
