a00e3908b29b4d7a609a1bfdc183d954c0ce6018
kirillpuzanov <k.puzanov@3divi.com>
2022-07-01 13:10:58 +0500

    Added main layout grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import AppRoutes from './app-routes';
+import theme from './theme';
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <ChakraProvider theme={theme}>
+      <AppRoutes />
+    </ChakraProvider>
+  );
 }
 
 export default App;
--- src/common/layouts/main-layout.components.tsx
+++ src/common/layouts/main-layout.components.tsx
@@ -1,5 +1,43 @@
+import {
+  HStack,
+  IconButton,
+  Grid,
+  Tooltip,
+  GridItem,
+  VStack,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { House } from 'phosphor-react';
+import { Outlet } from 'react-router-dom';
+
 function MainLayout() {
-  return null;
+  const navBg = useColorModeValue('gray.300', 'gray.600');
+  return (
+    <Grid
+      templateAreas='"header header" "nav main"'
+      gridTemplateRows="max-content 1fr"
+      gridTemplateColumns="max-content 1fr"
+      gap={0}
+      h="100vh"
+    >
+      <GridItem pl="2" bg="orange.300" area="header" h={14}></GridItem>
+      <GridItem pl="2" bg={navBg} area="nav" p={0} w={20}>
+        <VStack p={4}>
+          <Tooltip label="Home">
+            <IconButton
+              w="full"
+              as={House}
+              variant="ghost"
+              aria-label="Navigate to home page"
+            />
+          </Tooltip>
+        </VStack>
+      </GridItem>
+      <GridItem pl="2" bg="green.300" area="main">
+        <Outlet />
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default MainLayout;
